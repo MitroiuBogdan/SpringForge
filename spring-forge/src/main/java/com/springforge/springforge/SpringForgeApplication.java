24957005db003c97cf8922a899527805efb8b6b2
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2025-03-30 20:52:49 +0300

    Add dummy controller

--- spring-forge/src/main/java/com/springforge/springforge/SpringForgeApplication.java
+++ spring-forge/src/main/java/com/springforge/springforge/SpringForgeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringForgeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringForgeApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringForgeApplication.class, args);
+	}
 
 }
---
+++ spring-forge/src/main/java/com/springforge/springforge/controller/DummyController.java
@@ -0,0 +1,19 @@
+package com.springforge.springforge.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+public class DummyController {
+
+    @GetMapping("/dummy-response")
+    public CompletableFuture<DummyResponse> getDummyResponse() {
+        return CompletableFuture.supplyAsync(() ->
+            new DummyResponse("Success", 200, "Spring-Forge is optimized!")
+        );
+    }
+
+    public record DummyResponse(String status, int code, String message) {}
+}
