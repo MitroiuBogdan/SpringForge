26ff2979d9cf0a81014f764ebf0a5b06dd2f5c65
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2025-03-30 20:44:34 +0300

    Initialize the project

---
+++ spring-forge/src/main/java/com/springforge/springforge/SpringForgeApplication.java
@@ -0,0 +1,13 @@
+package com.springforge.springforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringForgeApplication.class, args);
+    }
+
+}
---
+++ spring-forge/src/test/java/com/springforge/springforge/SpringForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springforge.springforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
